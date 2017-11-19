f27769e6d7479676a4e464daedef52f76fb6b826
Alexandr <sanhiser@mail.ru>
2017-11-20 00:47:33 +0300

    task3

---
+++ docs/app.js
@@ -0,0 +1,43 @@
+
+var newUl = document.createElement("ul");
+root.appendChild(newUl);
+
+var newLi = document.createElement("li");
+newUl.appendChild(newLi);
+var span = document.createElement("span");
+var inText = document.createTextNode("Сделать задание #3 по web-программированию");
+span.appendChild(inText);
+newLi.appendChild(span);
+
+var btn = document.createElement("button");
+var insideText = document.createTextNode("Удалить");
+btn.appendChild(insideText);
+newLi.appendChild(btn);
+btn.onclick = function() {
+    newUl.removeChild(newLi);
+}
+
+var input = document.createElement("input");
+input.id = "add_task";
+root.appendChild(input);
+
+var but = document.createElement("button");
+but.id = "add_task";
+var intex = document.createTextNode("Добавить задачу");
+but.appendChild(intex);
+root.appendChild(but);
+but.onclick = function() {
+    var newLi = document.createElement("li");
+    newUl.appendChild(newLi);
+    var span = document.createElement("span");
+    var inText = document.createTextNode(document.getElementById('add_task').value);
+    span.appendChild(inText);
+    newLi.appendChild(span);
+    var btn = document.createElement("button");
+    var insideText = document.createTextNode("Удалить");
+    btn.appendChild(insideText);
+    newLi.appendChild(btn);
+    btn.onclick = function() {
+        newUl.removeChild(newLi);
+    }
+}
